fixed iou calculation

test_tf.py
@@ -1,4 +1,4 @@
-from utils import visualize_seg_detection
+from utils import visualize_seg_detection, compute_ap, compute_iou
 import numpy as np
 import tensorflow as tf
 from data_loaders.Ego2Hands_tf import Ego2HandsData
@@ -83,8 +83,8 @@ def test_tf(model: CSM_TF, config: Config, seq_i):
             #seg_output_final = nn.functional.interpolate(seg_output_final, size = (img_h, img_w), mode = 'bilinear', align_corners = True)
             seg_output_final = tf.image.resize(seg_output_final, size = (img_h, img_w))
 
-            #iou_np = compute_iou(seg_output_final, seg_gt_tensor)
-            #iou_meter.update(np.mean(iou_np), 1)
+            iou_np = compute_iou(seg_output_final, seg_gt_tensor)
+            iou_meter.update(np.mean(iou_np), 1)
             
             if config.energy:
                 energy_l_final = tf.image.resize(tf.expand_dims(energy_l_final, 0), size = (img_h, img_w))

train_tf.py
@@ -2,7 +2,7 @@ import numpy as np
 import tensorflow as tf
 from data_loaders.Ego2Hands_tf import Ego2HandsData
 import os
-from utils import AverageMeter
+from utils import AverageMeter, save_model
 import cv2
 from models.CSM.CSM_tf import CSM_baseline
 import cv2
@@ -74,8 +74,8 @@ def train(config, model):
 
         
     # Criterions
-    criterion_seg = tf.keras.losses.SparseCategoricalCrossentropy(from_logits = True) #  nn.CrossEntropyLoss().cuda()
-    criterion_mse = tf.keras.losses.MeanSquaredError() # nn.MSELoss().cuda()
+    criterion_seg = tf.keras.losses.SparseCategoricalCrossentropy(from_logits = True)
+    criterion_mse = tf.keras.losses.MeanSquaredError()
 
     # Measures
     iou_val_best = 0.0
@@ -95,11 +95,11 @@ def train(config, model):
         iters = 0
         max_iter = config.max_iter_seg
     
-    #optimizer_seg = torch.optim.Adam(model_seg.parameters(), lr_rate, weight_decay=config.weight_decay)
+    
     optimizer_seg = tf.keras.optimizers.Adam(learning_rate=lr_rate)
-    #lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer_seg, step_size = step_size, gamma = gamma)
+    
 
-    # model.sigmoid.build()
+
     _, _, img_tensor, _, _, _, _, _, _ = next(temp)
 
     model.build(input_shape = (config.batch_size, *img_tensor.shape))
@@ -211,24 +211,14 @@ def train(config, model):
                 
                 if model_is_best:
                     print("Saving best model at {}".format(model_save_path))
+                    save_model(model, True, False, model_save_path)
 
-
-                    # save_model({
-                            # 'iter': iters,
-                            # 'state_dict': model_seg.state_dict(),
-                    # }, is_best = True, is_last = False, filename = model_save_path)
-                
+            # after one step
             #print("Saving latest model at {}".format(model_save_path))
-            # save_model({
-                    # 'iter': iters,
-                    # 'state_dict': model_seg.state_dict(),
-            # }, is_best = False, is_last = False, filename = model_save_path)
-#             
-    # Save the last model as pretrained model
-    # save_model({
-         # 'iter': iters,
-         # 'state_dict': model_seg.state_dict(),
-    # }, is_best = False, is_last = True, filename = model_save_path)
+            #save_model(model, False, False, model_save_path)
+
+    # after training
+    save_model(model, False, True, model_save_path)
 
 
 if __name__=='__main__':

utils.py
@@ -28,12 +28,12 @@ class AverageMeter:
         
 def save_model(model: tf.keras.Model, is_best, is_last, filename):
     if is_last:
-        model.save(filename + '_pretrained.pth.tar')
+        model.save(filename + '_pretrained')
     else:
         if is_best:
-            model.save(filename + '_best.pth.tar')
+            model.save(filename + '_best')
         else:
-            model.save(filename + '_latest.pth.tar')
+            model.save(filename + '_latest')
 
 def Config(filename):
     with open(filename, 'r') as f:
@@ -48,9 +48,9 @@ def normalize_tensor(tensor, mean, std):
     return tensor
 
 def compute_iou(pred, target, box_l = None, box_r = None, is_idx = False):
-    n_classes = len(np.unique(target.cpu().data.numpy()))
+    n_classes = len(np.unique(target.numpy()))
     if n_classes == 1:
-         pred_unique_np = np.unique(pred.cpu().data.numpy())
+         pred_unique_np = np.unique(pred.numpy())
          if len(pred_unique_np) == 1 and pred_unique_np[0] == 0:
              return np.array([1.0])
          else:
@@ -76,16 +76,19 @@ def compute_iou(pred, target, box_l = None, box_r = None, is_idx = False):
                 pred_i[(pred_i == hand_label) & (region_mask == False)] = 0
                 target_i[(target_i == hand_label) & (region_mask == False)] = 0
         
-    pred = pred.view(-1)
-    target = target.view(-1)
+    pred = tf.reshape(pred, [-1])
+    target = tf.reshape(target, [-1])
 
     # Ignore IoU for background class ("0")
     for cls in range(1, n_classes):#xrange(1, n_classes):
         pred_inds = pred == cls
         #print(np.unique(pred_inds.cpu().data.numpy()))
         target_inds = target == cls
-        intersection = (pred_inds[target_inds]).long().sum().data.cpu().item()  # Cast to long to prevent overflows
-        union = pred_inds.long().sum().data.cpu().item() + target_inds.long().sum().data.cpu().item() - intersection
+        intersection = tf.reduce_sum(tf.cast(pred_inds[target_inds], tf.int64))  # Cast to long to prevent overflows
+        prediction_area = tf.reduce_sum(tf.cast(pred_inds, tf.int64))
+        gt_area = tf.reduce_sum(tf.cast(target_inds, tf.int64))
+    
+        union = prediction_area + gt_area - intersection
         if union == 0:
             ious.append(1.0)  # If there is no ground truth, do not include in evaluation
         else: